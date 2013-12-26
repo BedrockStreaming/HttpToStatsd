73f5f8369ed8e2d2856ca9fd9fb55b4408d70f1e
Olivier Mansour <olivier.mansour@m6.fr>
2013-12-26 16:13:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# installation
+
+```shell
+npm install 
+```
+
+... profit !
+
+# usage in production
+
+```
+node app
+```
+or if you want to choose the port (to run multiple instance) :
+```
+export NODE_PORT=8081 && node app 
+```
+
+# usage in dev (loading dev config)
+
+```
+export NODE_ENV=development
+node app
+```
+
+# usage in production (loading production config)
+
+```
+export NODE_ENV=production
+node app
+```
+
+## increment
+
+```
+/statsd/:node/increment?token=xxx
+```
+
+exemple
+
+```
+/statsd/raoul/increment?token=xxx
+```
+
+## timer
+
+```
+/statsd/:node/timer/:timing?token=xxx
+```
+
+exemple
+
+```
+/statsd/raoul/timer/234?token=xxx
+```
+
+timing value in ms
+
+## how to compute a token
+
+```
+md5sum(node + value + secretKey)
+```
+
+for increment, value is ```'1'```
+
+# test
+
+```
+npm test
+```
+
+# todo
+ * open source it
---
+++ test/security/securitytokenchecker.js
@@ -0,0 +1,103 @@
+(function() {
+
+    var sinon = require('sinon'),
+        sinonChai = require("sinon-chai"),
+        chai = require('chai'),
+        SecurityTokenChecker = require('../../lib/security/securitytokenchecker.js');
+
+    chai.use(sinonChai);
+    chai.should();
+
+    var mockedSecurity = {
+
+        getToken: function() {
+
+            return 'VALID_TOKEN';
+        }
+    };
+
+    var mockedResponse = {
+
+        status: function() {
+
+            return {
+                end: function() {}
+            };
+        }
+    };
+
+    describe('Test SecurityTokenChecker middleware', function() {
+
+        before(function() {
+
+            //define spies
+            sinon.spy(mockedSecurity, 'getToken');
+            sinon.spy(mockedResponse, 'status');
+
+        });
+
+        beforeEach(function() {
+            // reset spies
+            mockedResponse.status.reset();
+            mockedSecurity.getToken.reset();
+        });
+
+        describe('Test middleware with default value' , function() {
+            it('if req token different to Security getToken value should not call next method and send a 400 response', function() {
+
+                var next = sinon.spy();
+
+                var middleware = new SecurityTokenChecker(mockedSecurity);
+
+                middleware({query: {token:'INVALID_TOKEN' }, params: {node: 'NODE'}}, mockedResponse, next);
+
+                next.should.not.have.been.called;
+                mockedResponse.status.should.have.been.calledWith(400);
+            });
+
+            it('if req token equal to Security getToken value call next method and not send response', function() {
+
+                var next = sinon.spy();
+
+                var middleware = new SecurityTokenChecker(mockedSecurity);
+
+                middleware({query: {token:'VALID_TOKEN' }, params: {node: 'NODE'}}, mockedResponse, next);
+
+                next.should.have.been.called;
+                mockedResponse.status.should.not.have.been.called;
+            });
+        });
+
+        describe('Test middleware with valueParameter' , function() {
+            it('if req token different to Security getToken value should not call next method and send a 400 response', function() {
+
+                var next = sinon.spy();
+
+                var middleware = new SecurityTokenChecker(mockedSecurity, {valueParameter: 'test'});
+
+                middleware({query: {token:'INVALID_TOKEN' }, params: {node: 'NODE', test: 'VALUE'}}, mockedResponse, next);
+
+                next.should.not.have.been.called;
+
+                mockedSecurity.getToken.should.have.been.calledWith('NODE', 'VALUE');
+                mockedResponse.status.should.have.been.calledWith(400);
+            });
+
+            it('if req token equal to Security getToken value call next method and not send response', function() {
+
+                var next = sinon.spy();
+
+                var middleware = new SecurityTokenChecker(mockedSecurity, {valueParameter: 'test'});
+
+                middleware({query: {token:'VALID_TOKEN' }, params: {node: 'NODE', test: 'VALUE'}}, mockedResponse, next);
+
+                next.should.have.been.called;
+                mockedSecurity.getToken.should.have.been.calledWith('NODE', 'VALUE');
+                mockedResponse.status.should.not.have.been.called;
+            });
+        });
+
+    }) ;
+
+})();
+
