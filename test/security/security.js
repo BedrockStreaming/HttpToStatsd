f15ac49c2fd49a51a96259fe47ade768e42afb95
Pi Home Server <pihomeserver@gmail.com>
2015-10-23 16:04:36 +0200

    Update security tests

--- test/security/security.js
+++ test/security/security.js
@@ -5,11 +5,11 @@ var security = require('../../lib/security/security.js')('e56f5a32a32a65232');
 describe('service-log', function() {
     describe('getToken method', function(){
         it('should return a token based on the node, the value and a secret key', function(){
-          assert.equal('4e93d4885d32f2de7cc3641f4885c934', security.getToken('stats_counts.test',1));
+          assert.equal('4E93D4885D32F2DE7CC3641F4885C934', security.getToken('stats_counts.test',1));
         });
 
         it('should return a token based only on the node (no value)', function() {
-            assert.equal('c9307cb0a7a695b7f1258a669bd31c2b', security.getToken('stats_counts.test'));
+            assert.equal('C9307CB0A7A695B7F1258A669BD31C2B', security.getToken('stats_counts.test'));
         });
     });
 });
